7606648919969944e2264b52251d1513b7493a26
guspaz0 <gusti.paz@gmail.com>
2023-01-06 20:07:58 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Card from './components/Card.jsx'
+import Cards from './components/Cards.jsx'
+import SearchBar from './components/SearchBar.jsx'
+import characters, { Rick } from './data.js'
+import styles from './components/Card.module.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /*<img src="https://img.freepik.com/foto-gratis/hermosas-estrellas-brillantes-cielo-nocturno_181624-622.jpg" alt>*/
+    <div className='App' style={{ padding: '25px' }} >
+      <div className={styles.listItem}>
+        <Card
+          name={Rick.name}
+          species={Rick.species}
+          gender={Rick.gender}
+          image={Rick.image}
+          onClose={() => window.alert('Emulamos que se cierra la card')}
+        />
+      </div>
+      <hr />
+      <div>
+        <Cards
+          characters={characters}
+        />
+      </div>
+      <hr />
+      <div>
+        <SearchBar
+          onSearch={(characterID) => window.alert(characterID)}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
