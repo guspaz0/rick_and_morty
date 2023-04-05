6a149de03d802f9fa5c7fc51862a53c74b909851
guspaz0 <guspaz0>
2023-04-04 22:38:28 -0300

    actualizo

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,3 +1,25 @@
-const http = require('http')
+const { throws } = require('assert');
+const http = require('http');
+const PORT = 3001;
+const data = require("./utils/data")
 
-http.createServer(req)
+http.createServer((req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log(req.url);
+    try {
+        const {url} = req;
+        if (url.includes("/rickandmorty/character")) {
+            const id = url.split("/").at(-1);
+            console.log(id);
+            if (id) {
+                const character = data.filter((char) => char.id == id)
+                console.log(character)
+            }
+        }
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+})
+.listen(PORT, () => console.log(`server on port ${PORT}`))
